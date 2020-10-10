7da895166d7c772bfe0ba5d8fac274867c4c613f
TechnoCraftCons <44950527+TechnoCraftCons@users.noreply.github.com>
2020-10-10 12:39:53 -0500

    Adding java intro class

---
+++ src/javaIntro/Java01Basics.java
@@ -0,0 +1,38 @@
+package javaIntro;
+
+public class Java01Basics {
+    // This my single line comment
+    /* This is our main method that java will look to run/execute the code
+    this my second line comment
+    nknlksaf
+    nklksnfknfa multi line comment */
+    // Two types of errors 1. run time error 2. compile time error
+
+    public static void main(String[] args) {
+        //Java is always case sensitive
+        // What are data types
+        // Data type , variable , values
+        String a ="abc";
+        String b  = "abcde";
+
+        boolean abd = false;
+
+       // byte holds -128 to 127
+        byte ab = -128;
+
+        // short data type Its value-range lies between -32,768 to 32,767
+        short ac = -32768;
+
+        // int data type values range between - 2,147,483,648 (-2^31) to 2,147,483,647 (2^31 -1)
+        int c = 2147;
+        //long data type values range between -9,223,372,036,854,775,808(-2^63) to 9,223,372,036,854,775,807
+        long ca = 2147483648l;
+
+        float abc = 2147545.121245546561616456561651651651616f;
+
+        double abcd = 2147545.121245546561616456561651651651616;
+
+        System.out.println(abcd);
+    }
+}
+
---
+++ src/javaIntro/Java02ArthemeticOperators.java
@@ -0,0 +1,14 @@
+package javaIntro;
+
+public class Java02ArthemeticOperators {
+
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 0;
+        int result;
+
+        result = a / b;
+
+        System.out.println(result);
+    }
+}
